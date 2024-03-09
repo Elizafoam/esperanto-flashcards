039b1c3a690c0f5dec800503dc2d327e3f5d3919
Elizabeth J. <naeli.johnson@gmail.com>
2024-03-09 18:42:01 -0500

    set background and create card

---
+++ esperanto-flashcards/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div className='Card'>
+            <h5>{props.text}</h5>
+        </div>
+    )
+}
+
+export default Card;
